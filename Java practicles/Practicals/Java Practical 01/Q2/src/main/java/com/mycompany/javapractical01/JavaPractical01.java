27dbd874534466b246523252fefbc90763c1b615
J.A.N.S Jayasooriya <136626333+ANSJayasooriya@users.noreply.github.com>
2023-07-27 22:07:55 +0530

    Add files via upload

---
+++ Java practicles/Practicals/Java Practical 01/Q2/src/main/java/com/mycompany/javapractical01/JavaPractical01.java
@@ -0,0 +1,10 @@
+package com.mycompany.javapractical01;
+
+public class JavaPractical01
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Nadeeja Sasmitha");
+        System.out.println("Bachelor of information technology(Major in cyber secuirity)");
+    }
+}
